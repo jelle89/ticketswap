3f96e65ad6ff1a6eb66c59580a637e32f2486376
jelle89 <jelle.monen@hotmail.com>
2019-07-21 16:33:15 +0200

    lol

--- client/src/components/CommentContainer.js
+++ client/src/components/CommentContainer.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 export default function CommentContainer(props) {
+  console.log(props, 'propsvancomments')
   return (
     <form onSubmit={props.onSubmit}>
+    
       <label>
         Comment:
         <input
